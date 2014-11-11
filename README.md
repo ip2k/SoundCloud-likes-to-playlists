04d9fc6dfa157810891288719e8408247eae270f
Sean Petrow <spetrow@netflix.com>
2014-11-11 11:38:31 -0800

    Cleaned up code and docs a bit:

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 Likes to Playlists
 ===================
+I wrote because SoundCloud removed the ability to add tracks to playlists (sets) from the mobile client. I use this extensively, so I just scripted my own workflow.
+
+Here's the logic:
+- Get all playlists (sets)
+- Get all favorites (likes)
+- Select the playlist named as `Playlist_permalink_title` as `this_month_playlist`. Note that if multiple playlists are created with the same title, they're named as the downcase'd spaces-to-hyphens title, with things like apostrophes translated to hyphens for hte permalink. Beware.
+- If the playlist `Playlist_permalink_title` can't be found, create it as `Playlist_actual_title`. If you're not sure if your `Playlist_permalink_title` and `Playlist_actual_title` line up, test it first.
+- generate `favs_this_month` by cherry-picking favorites (from the most-recent `Page_size` ordered by `Pagination_order`) which are newer than `Favorites_to_add_date` (defaults: `200`, `created_at`, `DateTime.now.utc.beginning_of_month` (SC stores all dates as UTC))
+- If all of the items in `favs_this_month` exist in `this_month_playlist`, do nothing. This accounts for `this_month_playlist` having things not present in `favs_this_month`
+- If there are tracks IDs in `favs_this_month` not present in `this_month_playlist`, combine the track IDs from `favs_this_month` and `this_month_playlist` and update `this_month_playlist` on SoundCloud (and locally).
+- Display any tracks being added, along with `favs_this_month` and `this_month_playlist`
 
 How 2
 ======
--- likes-to-lists.rb
+++ likes-to-lists.rb
@@ -8,9 +8,17 @@ require 'active_support/core_ext'
 
 
 Config = YAML.load_file('oauth-config.yaml')  # OAuth User Credentials Flow. REF: https://developers.soundcloud.com/docs/api/guide#authentication
-page_size = 200  # pagination size. 200 is the max. REF: https://developers.soundcloud.com/docs/api/guide#pagination
+Page_size = 200  # pagination size. 200 is the max. REF: https://developers.soundcloud.com/docs/api/guide#pagination
+Pagination_order = 'created_at'  # an alternative would be 'last_modified'
+# note that SoundCloud represents this as Date.today.strftime("%B-%y").downcase ; e.g. => 'november-14'
+# whereas we create them as Date.today.strftime("%B '%y") ; e.g. => 'November '14'
+Playlist_actual_title = Date.today.strftime("%B '%y")
+Playlist_permalink_title = Date.today.strftime("%B-%y").downcase
+Favorites_to_add_date = DateTime.now.utc.beginning_of_month
 
-puts "Connecting to SoundCloud API..."
+
+
+puts 'Connecting to SoundCloud API...'
 # create client object with app and user credentials
 client = Soundcloud.new(:client_id => Config['client_id'],
                         :client_secret => Config['client_secret'],
@@ -18,38 +26,37 @@ client = Soundcloud.new(:client_id => Config['client_id'],
                         :password => Base64.decode64(Config['password_base64']))  # password is base64'd as not to store the passowrd in plaintext. Using encryption here would be ~pointless.
 
 # get playlists from SC
-puts "Getting your playlists..."
-playlists = client.get("/me/playlists", :limit => page_size)
+puts 'Getting your playlists...'
+playlists = client.get("/me/playlists", :limit => Page_size, :order => Pagination_order)
 
-puts "Getting your favorites..."
-favs = client.get("/me/favorites", :limit => page_size)
+puts 'Getting your favorites...'
+favs = client.get("/me/favorites", :limit => Page_size, :order => Pagination_order)
 
 
 # see if the playlists already exists
-# note that SoundCloud represents this as Date.today.strftime("%B-%y").downcase ; e.g. => 'november-14' 
-# whereas we create them as Date.today.strftime("%B '%y") ; e.g. => 'November '14'
-this_month_playlist = playlists.select {|i| i.permalink == (Date.today.strftime("%B-%y").downcase) }.first
+
+this_month_playlist = playlists.select {|i| i.permalink == (Playlist_permalink_title) }.first
 
 # create playlists if it doesn't exist
 unless this_month_playlist
-  print "playlist #{Date.today.strftime("%B-%y").downcase} not found, will create it..."
+  print "playlist #{Playlist_permalink_title} not found, will create it..."
   begin
     this_month_playlist = client.post("/me/playlists", 
       :playlist => { 
-        :title => Date.today.strftime("%B '%y"), 
+        :title => Playlist_actual_title,
         :sharing => 'public' 
       } 
     )
-    puts "Created."
+    puts 'Created !!!'
   rescue
-    abort("Playlist creation failed. Please create #{Date.today.strftime("%B '%y")} manually and try again.")
+    abort "Playlist creation failed. Please create #{Playlist_actual_title} manually and try again."
   end
 end
 
 # Create a list of tracks CREATED this month
 # NOTE NOTE NOTE: if the track was created more than a month ago, consider changing this to i.last_modified
 # however, last_modified brings a different set of issues, as tracks could be modified
-favs_this_month = favs.select {|i| i.created_at > DateTime.now.utc.beginning_of_month }
+favs_this_month = favs.select {|i| i.created_at > Favorites_to_add_date }
 
 
 # checks to see if each favorite track ID is included in an array of [this_month_playlist track IDs]
@@ -62,29 +69,30 @@ favs_this_month = favs.select {|i| i.created_at > DateTime.now.utc.beginning_of_
 unless favs_this_month.map(&:id).all? {|track_id| this_month_playlist.tracks.map(&:id).include?(track_id) }
   # :tracks is a unique set of favs_this_month IDs and this_month_playlist IDs in format [{:id => XXXX}, {:id => YYYY}, ... ]
   begin
-    puts ">>>>> Not all of your favorites are already on the playlist this month! Adding them... <<<<<"
-    puts "Tracks to Add"
-    puts "==============="
+    puts '>>>>> Not all of your favorites are already on the playlist this month! Adding them... <<<<<'
+    puts 'Tracks to Add'
+    puts '==============='
     ap (favs_this_month - this_month_playlist.tracks).map(&:title)
-    client.put(this_month_playlist.uri,
+    this_month_playlist = client.put(this_month_playlist.uri,
       :playlist => {
         :tracks => (favs_this_month.map(&:id) + this_month_playlist.tracks.map(&:id)).uniq.map { |id| {:id => id} }
       }
     )
-    puts "OK !!!"
+    puts 'OK !!!\r\n'
+    puts
   rescue
     ap this_month_playlist
     ap favs_this_month
-    abort("Couldn't update playlist. this_month_playlist and favs_this_month are above.")
+    abort 'Couldn\'t update playlist. this_month_playlist and favs_this_month are above.'
   end
 else
-  puts ">>>>> It looks like everything you've favorited is already in the playlist for this month. <<<<<"
+  puts '>>>>> It looks like everything you\'ve favorited is already in the playlist for this month. <<<<<'
 end
 
-puts "Favorites"
-puts "==========="
+puts 'Favorites'
+puts '==========='
 ap favs_this_month.map(&:title)
 puts "#{Date.today.strftime("%B '%y")} (your Playlist AKA Set)"
-puts "==========================================="
+puts '==========================================='
 ap this_month_playlist.tracks.map(&:title)
-puts "Done !!!"
+puts 'Done !!!'
